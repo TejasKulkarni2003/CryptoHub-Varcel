9fec09295b85efc52ef19ca43b486522f6609ccd
TejasKulkarni2003 <104378810+TejasKulkarni2003@users.noreply.github.com>
2023-06-30 19:04:12 +0530

    header improvements 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ import { Button, HStack } from '@chakra-ui/react';
 const Header = () => {
   return (
     <>
-        <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"} justifyContent={'space-evenly'} style={{background: "linear-gradient(#03030366, #195b5a);"}}>
-            <Button variant={"unstyled"} color={"white"}>
+        <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"} justifyContent={'space-evenly'} style={{backgroundImage: "linear-gradient(#03030366,#195b5a)"}}>
+            <Button variant={"unstyled"} color={"white"} borderBottom={"2px solid"}>
                 <Link to="/">Home</Link>
             </Button>
-            <Button variant={"unstyled"} color={"white"}>
+            <Button variant={"unstyled"} color={"white"} borderBottom={"2px solid"}>
                 <Link to="/exchanges">Exchanges</Link>
             </Button>
-            <Button variant={"unstyled"} color={"white"}>
+            <Button variant={"unstyled"} color={"white"} borderBottom={"2px solid"}>
                 <Link to="/coins">Coins</Link>
             </Button>
         </HStack>
